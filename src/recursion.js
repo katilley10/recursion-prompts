3532cf41144a4b47feac922b97a0f343da4e1f77
kyle tilley <katilley10@gmail.com>
2020-03-09 21:32:46 -0600

    this didn't work

--- src/recursion.js
+++ src/recursion.js
@@ -19,9 +19,15 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+    let i = -1;
     if (array.length === 0) {
         return 0;
-    } 
+    } else if (array.length > 0) {
+        console.log(i);
+       let current = array[i];
+       i++; 
+       return array[i] + sum(array);
+    }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
