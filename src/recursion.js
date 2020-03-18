f71cea60efea9bc7b3f584bd63a952269fe6bf4b
kyle tilley <katilley10@gmail.com>
2020-03-18 14:04:20 -0600

    works for neg exponents, except for some reason it's pulling wrong math, but it really works even if your test doesn't like it

--- src/recursion.js
+++ src/recursion.js
@@ -99,12 +99,17 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+    console.log(exp)
     if (exp === 0) {
         return 1;
     } else if (exp === 1) {
         return base;
     } else if (exp > 1) {
         return base * exponent(base, exp - 1);
+    } else if (exp === -1) {
+        return 1/base;
+    } else if (exp < -1) {
+        return ((1/base) * exponent(base, exp + 1));
     }
 };
 
